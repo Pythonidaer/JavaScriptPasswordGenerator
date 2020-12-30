163d24ae02d11d817faf8501f4affa8dd00e7c32
Pythonidaer <pythonidaer@gmail.com>
2020-12-29 21:26:25 -0500

    First draft of dark mode, concept is complete but needs better styling and cleaner code

---
+++ assets/scripts/darkmode.js
@@ -0,0 +1,18 @@
+var toggler = document.querySelector('input[name=theme]');
+
+toggler.addEventListener('change', function() {
+    if(this.checked) {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+})
+
+let trans = () => {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+    }, 1000)
+}
